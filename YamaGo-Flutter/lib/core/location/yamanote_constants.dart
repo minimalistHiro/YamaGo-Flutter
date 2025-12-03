c39e6617cca6ca0b2b291616a502f4624f8ecffd
hiro <h.kaneko.baseball@icloud.com>
2025-12-03 20:43:04 +0900

    Adjust Yamanote map bounds

--- YamaGo-Flutter/lib/core/location/yamanote_constants.dart
+++ YamaGo-Flutter/lib/core/location/yamanote_constants.dart
@@ -1,11 +1,11 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-const LatLng yamanoteCenter = LatLng(35.735, 139.725);
+const LatLng yamanoteCenter = LatLng(35.69, 139.73);
 
 final LatLngBounds yamanoteBounds = LatLngBounds(
-  southwest: LatLng(35.60, 139.63),
-  northeast: LatLng(35.88, 139.82),
+  southwest: LatLng(35.639, 139.707),
+  northeast: LatLng(35.73, 139.77),
 );
 
-const double yamanoteMinZoom = 12.5;
+const double yamanoteMinZoom = 14.5;
 const double yamanoteMaxZoom = 19.0;
